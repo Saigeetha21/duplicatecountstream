c9fb78f83812f83263d73d972f52ed02cc93f6b2
Saigeetha@21 <geethachandaka21@gmail.com>
2025-09-05 16:21:28 +0530

    ascending order

--- src/StreamCodes/Ascending.java
+++ src/StreamCodes/Ascending.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Ascending {
-
+//
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(1,2,4,5,6,3);
 		List<Integer> num = numbers.stream().sorted((n1,n2)->n1.compareTo(n2)).collect(Collectors.toList());
